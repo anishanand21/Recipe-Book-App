0cd1fa4ac144045adb143443828e3e87bfe5aa8e
Anish <anishanand3733@gmail.com>
2021-02-03 00:01:31 +0530

    SearchView Toolbar

--- app/src/main/java/com/android/recipebook/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/android/recipebook/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -27,15 +27,15 @@ class RecipeListViewModel
     val query = mutableStateOf("Chicken")
 
     init {
-        newSearch()
+        newSearch(query.value)
     }
 
-    fun newSearch() {
+    fun newSearch(query: String) {
         viewModelScope.launch {
             val result = repository.search(
                 token = token,
                 page = 1,
-                query = "chicken"
+                query = query
             )
             recipes.value = result
         }
