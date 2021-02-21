826325c15ee874b6faecf427b997de3a57fbb907
Anish <anishanand3733@gmail.com>
2021-02-22 00:46:36 +0530

    Sending data from fragment to fragment (without safe args)

---
+++ app/src/main/java/com/android/recipebook/presentation/components/RecipeList.kt
@@ -0,0 +1,83 @@
+package com.android.recipebook.presentation.components
+
+import android.nfc.tech.MifareUltralight
+import android.os.Bundle
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.android.recipebook.R
+import com.android.recipebook.domain.model.Recipe
+import com.android.recipebook.presentation.components.util.SnackbarController
+import com.android.recipebook.presentation.ui.recipe_list.RecipeListEvent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+
+@ExperimentalMaterialApi
+@ExperimentalCoroutinesApi
+@Composable
+fun RecipeList(
+    loading: Boolean,
+    recipes: List<Recipe>,
+    onChangeRecipeScrollPosition: (Int) -> Unit,
+    page: Int,
+    onNextPage: (RecipeListEvent) -> Unit,
+    scaffoldState: ScaffoldState,
+    snackbarController: SnackbarController,
+    navController: NavController,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+    ) {
+        if (loading && recipes.isEmpty()) {
+            LoadingRecipeListShimmer(imageHeight = 250.dp)
+        } else {
+            LazyColumn {
+                itemsIndexed(
+                    items = recipes
+                ) { index, recipe ->
+                    onChangeRecipeScrollPosition(index)
+                    if ((index + 1) >= (page * MifareUltralight.PAGE_SIZE) && !loading) {
+                        onNextPage(RecipeListEvent.NextPageEvent)
+                    }
+                    RecipeCard(
+                        recipe = recipe,
+                        onClick = {
+                            if (recipe.id != null) {
+                                val bundle = Bundle()
+                                bundle.putInt("recipeId", recipe.id)
+                                navController.navigate(R.id.viewRecipe, bundle)
+                            } else {
+                                snackbarController.getScope().launch {
+                                    snackbarController.showSnackbar(
+                                        scaffoldState = scaffoldState,
+                                        message = "Recipe Error",
+                                        actionLabel = "Ok"
+                                    )
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        }
+        CircularIndeterminateProgressBar(isDisplayed = loading)
+        DefaultSnackbar(
+            snackbarHostState = scaffoldState.snackbarHostState,
+            onDismiss = {
+                scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+            },
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
+    }
+}
--- app/src/main/java/com/android/recipebook/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/android/recipebook/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,51 +1,20 @@
 package com.android.recipebook.presentation.ui.recipe_list
 
-import android.app.Application
-import android.nfc.tech.MifareUltralight.PAGE_SIZE
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.ScrollableRow
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBalanceWallet
-import androidx.compose.material.icons.filled.BrokenImage
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Brush.Companion.linearGradient
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.findNavController
-import com.android.recipebook.R
+import androidx.navigation.fragment.findNavController
 import com.android.recipebook.presentation.BaseApplication
 import com.android.recipebook.presentation.components.*
-import com.android.recipebook.presentation.components.HeartAnimationDefinition.HeartButtonState.*
 import com.android.recipebook.presentation.components.util.SnackbarController
 import com.android.recipebook.presentation.theme.AppTheme
 import com.android.recipebook.presentation.ui.recipe_list.RecipeListEvent.*
-import com.android.recipebook.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
@@ -121,35 +90,18 @@ class RecipeListFragment : Fragment() {
                             scaffoldState.snackbarHostState
                         }
                     ) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .background(MaterialTheme.colors.background)
-                        ) {
-                            if (loading && recipes.isEmpty()) {
-                                LoadingRecipeListShimmer(imageHeight = 250.dp)
-                            } else {
-                                LazyColumn {
-                                    itemsIndexed(
-                                        items = recipes
-                                    ) { index, recipe ->
-                                        viewModel.onChangeRecipeScrollPosition(index)
-                                        if ((index + 1) >= (page * PAGE_SIZE) && !loading) {
-                                            viewModel.onTriggerEvent(NextPageEvent)
-                                        }
-                                        RecipeCard(recipe = recipe, onClick = {})
-                                    }
-                                }
-                            }
-                            CircularIndeterminateProgressBar(isDisplayed = loading)
-                            DefaultSnackbar(
-                                snackbarHostState = scaffoldState.snackbarHostState,
-                                onDismiss = {
-                                    scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
-                                },
-                                modifier = Modifier.align(Alignment.BottomCenter)
-                            )
-                        }
+                        RecipeList(
+                            loading = loading,
+                            recipes = recipes,
+                            onChangeRecipeScrollPosition = viewModel::onChangeRecipeScrollPosition,
+                            page = page,
+                            onNextPage = {
+                                viewModel.onTriggerEvent(NextPageEvent)
+                            },
+                            scaffoldState = scaffoldState,
+                            snackbarController = snackbarController,
+                            navController = findNavController()
+                        )
                     }
                 }
             }
